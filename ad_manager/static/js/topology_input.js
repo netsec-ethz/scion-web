a33166b687e4cad1fba840a54850c30f2a58291b
FR4NK-W <wirzf@student.ethz.ch>
2016-06-22 08:42:56 +0200

    enhance form reloading behaviour
    also reload multiple entries per service type
    move JS toggle_input to static/js/topology_input.js

--- ad_manager/static/js/topology_input.js
+++ ad_manager/static/js/topology_input.js
@@ -12,6 +12,44 @@ function parseTopology(reloadedTopology) {
     return reloadedTopology;
 }
 
+function toggleInput(elem) {
+        var accordion = $(elem).parents('.accordion');
+        var currentClass = $(elem).children(':first').attr('class'); // either glyphicon glyphicon-{plus|minus}
+        if (currentClass === 'glyphicon glyphicon-plus') {
+            var clone = $(elem).parents('.item').clone();
+            // update clone id
+            var oldId = clone.attr('id').split('-');
+            var idName = oldId[0];
+            var newIdNo = parseInt(oldId[1]) + 1;
+            $(clone).attr('id', idName + '-' + newIdNo.toString());
+            // increment current name for next field
+            var currentName = $(clone).find( '.server-name-input' ).attr('value');
+            var nameParts = currentName.split('-');
+            nameParts[nameParts.length-1]++;
+            var newName = nameParts.join('-');
+            $(clone).find('.server-name-input').attr('value', newName);
+            // reset IP
+            $(clone).find( '.server-address-input' ).val('');
+            // append the cloned and cleaned item
+            accordion.append(clone);
+            //var test = $(clone).children(':first').prop('tagName');
+            if ($(clone).children(':first').attr('class') !== 'doc-hr') {
+                // add separator
+                $(clone).prepend('<div class="doc-hr"></div>');
+            }
+            $(elem).removeClass('btn-success').addClass('btn-danger');
+            $(elem).children(':first').removeClass('glyphicon glyphicon-plus').addClass('glyphicon glyphicon-minus');
+        } else {
+            var sibling = $(elem).parents('.item').next();
+            var firstItem = accordion.children(':nth-child(2)');
+            var selfItem = $(elem).parents('.item');
+            if (firstItem.is(selfItem)) {
+                $(sibling).children(':first').remove();
+            }
+            selfItem.remove();
+        }
+    }
+
 function setLoadedTopology(reloadedTopology) {
     var isCore = reloadedTopology['Core'];
     $('#inputIsCore.shownCheckbox').prop('checked', isCore);
@@ -33,14 +71,24 @@ function setLoadedTopology(reloadedTopology) {
                 type = 'Domain';
             }
             //var typeValue = type.toLowerCase() + '_server';
-            //$('#input'+type+'ServerType').attr('value', typeValue); // already set
-            var name = Object.keys(entry)[0];
-            $('#input'+type+'ServerName').attr('value', name);
-            var server = entry[name];
-            var address = server['Addr'];
-            $('#input'+type+'ServerAddress').attr('value', address);
-            var port = server['Port'];
-            $('#input'+type+'ServerPort').attr('value', port);
+            //$('#input'+type+'ServerType').attr('value', typeValue); // typeValue already set in template
+            var names = Object.keys(entry); // get a list of keys
+
+            for (var i in names) {
+                var name = names[i];
+                if (i > 0) {
+                    // if more than one entry, create additional form input
+                    $('.' + type + 'Item'+':last').find('.btn-success').click()
+                }
+                // fill form values
+                var itemSelector = '#' + type + 'Item-' + (parseInt(i) + 1).toString();
+                $(itemSelector + ' #input'+type+'ServerName').val(name);
+                var server = entry[name];
+                var address = server['Addr'];
+                $(itemSelector + ' #input'+type+'ServerAddress').val(address);
+                var port = server['Port'];
+                $(itemSelector + ' #input'+type+'ServerPort').val(port);
+            }
 
             // remove entry
             delete reloadedTopology[entryKey]
