06e147a27baeee8d9d0ba57e0ffe8b41425962ec
FR4NK-W <wirzf@student.ethz.ch>
2016-07-01 23:41:50 +0200

    hostfile generator full refactoring
    extracted reusable parts
    more expressive code

--- ad_manager/util/hostfile_generator.py
+++ ad_manager/util/hostfile_generator.py
@@ -16,6 +16,45 @@ WEB_ROOT = os.path.join(PROJECT_ROOT, 'sub', 'web')
 SUPPORTED_CLOUD_ENGINES = ['switch_engines', 'amazon_ec2']
 
 
+def add_new_section(config, section_name):
+    try:
+        config.add_section(section_name)
+    except configparser.DuplicateSectionError:
+        pass  # section already exists
+
+
+def fill_section(config, section_name, val, tags, hostname):
+    server_index = 0
+    add_new_section(config, section_name)
+    for entry in val:
+        server_index += 1
+        entry = entry.split('/')[0]  # remove subnet size
+        config[section_name][entry] = tags + '={} # {}'.format(server_index,
+                                                               hostname)
+
+
+def set_cloud_providers(config, topology_params):
+    try:
+        addresses = topology_params.getlist('inputCloudAddress')
+        providers = topology_params.getlist('inputCloudEngine')
+        for provider in SUPPORTED_CLOUD_ENGINES:
+            if provider in providers:
+                add_new_section(config, provider)
+                # a direct mask would be more efficient
+                section_values = filter(
+                    None, map(lambda matched:
+                              matched[0] if matched[1] == provider else None,
+                              zip(addresses, providers)
+                              )
+                )
+                for ip in section_values:
+                    config.set(provider, ip)
+    except KeyError:
+        # There are no IPs with a selected cloud provider so the previous
+        # section is superfluous
+        pass
+
+
 def generate_ansible_hostfile(topology_params, isd_as):
     """
     Generate the host file for Ansible
@@ -43,77 +82,39 @@ def generate_ansible_hostfile(topology_params, isd_as):
                               ('ZookeeperServer', 'zookeeper_service')]:
         val = topology_params.getlist('input' + key + 'Address')
         hostname = topology_params['input' + key + 'Name']
-        server_index = 0
-        for entry in val:
-            server_index += 1
-            entry = entry.split('/')[0]  # remove subnet size
-            section_name = None
-            if service_type.endswith('_server'):
-                section_name = service_type + 's'
-                try:
-                    config.add_section(section_name)
-                except configparser.DuplicateSectionError:
-                    pass  # section already exists
-                config[section_name] = \
-                    {entry: 'isd={} as={} {}={} # {}'.format(isd_id, as_id,
-                                                             lkp[service_type],
-                                                             server_index,
-                                                             hostname)}
-            elif service_type == 'router':
-                remote_isd, remote_as = topology_params[
-                    'inputInterfaceRemoteName'].split('-')
-                section_name = 'edge_routers'
-                try:
-                    config.add_section(section_name)
-                except configparser.DuplicateSectionError:
-                    pass  # section already exists
-                config[section_name][entry] = \
-                    'isd={} as={} to_isd={}' \
-                    ' to_as={} {}={} # {}'.format(isd_id,
-                                                  as_id,
-                                                  remote_isd,
-                                                  remote_as,
-                                                  lkp[service_type],
-                                                  server_index,
-                                                  hostname)
-            elif service_type == 'zookeeper_service':
-                config['zookeepers'] = \
-                    {entry: 'isd={} as={} {}={} # {}'.format(isd_id, as_id,
-                                                             lkp[service_type],
-                                                             server_index,
-                                                             hostname)}
-        if section_name is not None:
-            scion_nodes.append(section_name)
+        if service_type.endswith('_server'):
+            section_name = service_type + 's'
+            tags = 'isd={} as={} {}'.format(isd_id, as_id,
+                                            lkp[service_type])
+            fill_section(config, section_name, val, tags, hostname)
+        elif service_type == 'router':
+            remote_isd, remote_as = topology_params[
+                'inputInterfaceRemoteName'].split('-')
+            section_name = 'edge_routers'
+            tags = 'isd={} as={} to_isd={}' \
+                ' to_as={} {}'.format(isd_id,
+                                      as_id,
+                                      remote_isd,
+                                      remote_as,
+                                      lkp[service_type])
+            fill_section(config, section_name, val, tags, hostname)
+        elif service_type == 'zookeeper_service':
+            section_name = 'zookeepers'
+            tags = 'isd={} as={} {}'.format(isd_id,
+                                            as_id,
+                                            lkp[service_type])
+            fill_section(config, section_name, val, tags, hostname)
+            continue  # zookeepers are not to be listed in scion_nodes children
+        scion_nodes.append(section_name)
 
-    config['scion_nodes:children'] = {}
+    add_new_section(config, 'scion_nodes:children')
     for role in scion_nodes:
         config.set('scion_nodes:children', role)
 
-    # cloud providers sections
-    try:
-        addresses = topology_params.getlist('inputCloudAddress')
-        providers = topology_params.getlist('inputCloudEngine')
-        for provider in SUPPORTED_CLOUD_ENGINES:
-            if provider in providers:
-                try:
-                    config.add_section(provider)
-                except configparser.DuplicateSectionError:
-                    pass  # section already exists
-                # a direct mask would be more efficient
-                section_values = filter(
-                    None, map(lambda matched:
-                              matched[0] if matched[1] == provider else None,
-                              zip(addresses, providers)
-                              )
-                )
-                for ip in section_values:
-                    config.set(provider, ip)
-    except KeyError:
-        # There are no IPs with a selected cloud provider so the previous
-        # section is superfluous
-        pass
+    # set cloud providers sections
+    set_cloud_providers(config, topology_params)
 
-    config['scion_nodes:vars'] = {}
+    add_new_section(config, 'scion_nodes:vars')
     local_gen_path = os.path.join(WEB_ROOT, 'gen')
     config.set('scion_nodes:vars', 'local_gen={}'.format(local_gen_path))
 
